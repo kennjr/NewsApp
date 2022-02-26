c7672bbf20c24981a72c45c211f8d05a00224046
kennjr <ken.juniour@student.moringaschool.com>
2022-02-26 10:09:11 +0300

    First heroku commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_ken():
+    return "Hello Ken"
+
+if __name__ == "__main__":
+    app.run()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
