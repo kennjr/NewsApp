6a58fd68b42c4db30326dd463382730fec4b03d2
kennjr <ken.juniour@student.moringaschool.com>
2022-02-26 12:24:32 +0300

    Created the top-headlines url

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ class Config:
     API_KEY = "a3121d3568944bf782807ac90b13b1f7"
     """This is for the base url that we'll use to make requests to the news api"""
     NEWS_API_BASE_URL = "https://newsapi.org/v2"
-
+    ADD_API_KEY = "&apiKey=a3121d3568944bf782807ac90b13b1f7"
+    TOP_HEADLINES_URL = NEWS_API_BASE_URL + "/top-headlines?country=us" + ADD_API_KEY
     pass
 
 
